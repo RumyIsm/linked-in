79c9864aebe8523e8d8fce2948f7652988a961dd
Rumejsa <rumejsaismani.edumind@gmail.com>
2025-01-23 16:29:49 +0100

    i18next instalation

--- linked-in/src/firstPage/FirstPage.jsx
+++ linked-in/src/firstPage/FirstPage.jsx
@@ -2,10 +2,16 @@ import React from "react";
 import { useNavigate } from "react-router-dom";
 import GettingStarted from "../assets/first_page photo.svg";
 import LinkedinLogo from "../assets/linkedinLogo.png";
+import { useTranslation } from "react-i18next";
 import "./FirstPage.css";
 
 function FirstPage() {
   let navigate = useNavigate();
+  const [t, i18n] = useTranslation ("global")
+  const changeLang = (lang) => {
+    i18n.changeLanguage(lang)
+    localStorage.setItem("language", lang);
+  };
   return (
     <div className="get_start">
       <div className="navbarr">
@@ -17,10 +23,12 @@ function FirstPage() {
         Linked
         <img src={LinkedinLogo} className="linkedinLogo" alt="LinkedIn Logo" />
       </h1>
+      <button onClick={ () => changeLang("en")}>{t("language.en")}</button>
+      <button  onClick={ () => changeLang("al")}>{t("language.al")}</button>
       </div>
       <div className="welcome">
       <div className="welcome_1">
-      <h1 className="welcome_content"> Welcome to your professional community </h1>
+      <h1 className="welcome_content"> {t("header")} </h1>
       <div className="welcome_buttons">
       <button className="getting-started" onClick={() =>{
           console.log("Navigating to /login");
--- linked-in/src/main.jsx
+++ linked-in/src/main.jsx
@@ -5,11 +5,31 @@ import { router } from "./routes";
 import { ToastContainer } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
 import "react-quill/dist/quill.snow.css";
+import i18next from "i18next";
+import global_en from "../src/locales/en/global.json"
+import global_al from "../src/locales/al/global.json"
+import { I18nextProvider } from "react-i18next";
 import "./index.css";
+
+const savedLanguage = localStorage.getItem("language") || "en";
+i18next.init({
+  interpolation: { escapeValue: false },
+  lng: savedLanguage,
+  resources: {
+    en: {
+      global: global_en,
+    },
+    al: {
+      global: global_al,
+    },
+  },
+});
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
+    <I18nextProvider i18n={i18next}>
     <RouterProvider router={router} />
     <ToastContainer />
+    </I18nextProvider>
     </React.StrictMode>
 );
